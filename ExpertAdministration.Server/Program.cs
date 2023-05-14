754c560e9e8d0793e5770919ec990fb59c9d53ce
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2023-05-14 15:10:01 +0300

    Remove boilerplate
    Removed all the boilerplate code from the api

--- ExpertAdministration.Server/Program.cs
+++ ExpertAdministration.Server/Program.cs
@@ -6,10 +6,7 @@ var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
     {
